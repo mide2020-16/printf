7c8577cd2e132a3bf66c475772fe7375fc0814b7
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2024-02-14 14:10:48 +0100

    Create _strlen.c

---
+++ _strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: string to check
+ *
+ * Return: length of string
+ */
+int _strlen(const char *s)
+{
+	int i, size = 0;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		++size;
+	}
+	return (size);
+}
