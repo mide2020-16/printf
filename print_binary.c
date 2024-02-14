483d382ce48b0b85d3042a9a1a4b32d5e740b8fe
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2024-02-14 14:07:39 +0100

    Update print_binary.c

--- print_binary.c
+++ print_binary.c
@@ -1,21 +1,48 @@
+#include <stdarg.h>
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * print_binary - prints the binary representation of a number
- * @number: number to print in binary
- * Return: void
+ * print_binary - print a decimal number in binary form
+ * @args: decimal number to print binary from
+ *
+ * Return: number of character printed
  */
-
-int print_binary(unsigned int number)
+int print_binary(va_list args)
 {
-  int count = 0;
+	int d, printed, binary;
 
-  if (number > 1)
-  {
-		count += print_binary(number >> 1);
-  }
-	_putchar((number & 1) + '0');
+	d = va_arg(args, int);
 
-  return (count + 1);
+
+	binary = convert_dec_to_bin(d);
+
+	printed = count_number(binary);
+
+	print_number(binary);
+
+	return (printed);
+}
+
+/**
+ * convert_dec_to_bin - converts decimal to binary
+ * @n: number to convert
+ *
+ * Return: returns binary value
+ */
+int convert_dec_to_bin(int n)
+{
+	int binary = 0, remainder, decimal, position = 1;
+
+
+	decimal = n;
+
+	while (decimal > 0)
+	{
+		remainder = decimal % 2;
+		decimal /= 2;
+		binary += remainder * position;
+		position *= 10;
+	}
+
+	return (binary);
 }
