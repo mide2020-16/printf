20e1aceab6a52267d10ebe3fe4a3402300c10517
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2024-02-14 13:58:22 +0100

    Update print_integer.c

--- print_integer.c
+++ print_integer.c
@@ -1,83 +1,27 @@
+#include <stdarg.h>
 #include "main.h"
-#include <unistd.h>
-#include <limits.h>
-
-#define BUFFER_SIZE 1024 /* Define the buffer size */
 
 /**
- * print_integer - Prints an integer
- * @number: The integer
- * @flag_plus: The plus flag
- * @flag_space: The space flag
- * @field_width: The field width
- * Return: The number of characters printed
+ * print_integer - prints a integer from va_list args
+ * @args: arguments to print integer from
+ *
+ * Return: number of character printed
  */
-int print_integer(int number, int flag_plus, int flag_space, int field_width)
+int print_integer(va_list args)
 {
-  int count = 0;
-  int i, j, temp, f, buf_count = 0;
-  char buffer[BUFFER_SIZE];
+	int d, printed;
 
-  temp = number;
-  /*is_int_min = 0;
-  
-  if (temp == INT_MIN)
-  {
-    is_int_min = 1;
-    temp++;
-  }*/
-  /* Check if the temporary integer is negative */
-  if (temp < 0)
-  {
-    write(1, "-", 1);
-    temp = -temp;
-    count++;
-  }
+	d = va_arg(args, int);
 
-  /* Print spaces for the field width */
-  for (f = 0; f < field_width; f++)
-  {
-    write(1, " ", 1);
-    count++;
-  }
+	printed = count_number(d);
 
-  /* Check if the plus flag is set */
-  if (flag_plus == 1)
-  {
-    write(1, "+", 1);
-    count++;
-  }
+	if (d < 0)
+	{
+		_putchar('-');
+		++printed;
+	}
+	d = -(d);
+	print_number(d);
 
-  /* Check if the space flag is set */
-  if (flag_space == 1)
-  {
-    write(1, " ", 1);
-    count++;
-  }
-
-  /* Convert the temporary integer to a string in reverse order and store it in the buffer */
-  do
-  {
-    if (buf_count < BUFFER_SIZE)
-    {
-      buffer[buf_count++] = (temp % 10) + '0';
-    }
-    temp /= 10;
-  } while (temp > 0);
-
-  /* Reverse the string in the buffer */
-  for (i = 0, j = buf_count - 1; i < j; i++, j--)
-  {
-    char temp = buffer[i];
-    buffer[i] = buffer[j];
-    buffer[j] = temp;
-  }
-
-  /* Write the buffer content */
-  write(1, buffer, buf_count);
-
-  /* Update the total count */
-  count += buf_count;
-
-  return count;
+	return (printed);
 }
