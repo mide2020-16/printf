27c500aba6040355c7080dc3f5dc8ea057b222d0
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2024-02-14 14:13:18 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,14 +1,36 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
-#include <unistd.h>
-#include <stddef.h>
 
+/**
+ * struct specifier_formats - struct holding the specifier format for printf
+ * @format: character indicating format to be used
+ * @f: function to be returned based on function
+ */
+typedef struct specifier_formats
+{
+	char format;
+	int (*f)(va_list args);
+} sp_f;
+
+int _strlen(const char *s);
 int _printf(const char *format, ...);
-int _putchar(char character);
-int print_string(char *string);
-int print_integer(int number, int flag_plus, int flag_space, int field_width);
-int print_binary(unsigned int number);
-int print_unsigned_int(unsigned int integer, int flag_hash, int field_width);
-#endif /*MAIN_H*/
+void _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_decimal(va_list args);
+int (*get_format_function(char c))(va_list args);
+int print_integer(va_list args);
+int print_number(int n);
+int fmt_arr_checker(int fmt_arr_size, char formats_array[], char character);
+int print_percent(va_list args);
+int count_number(int n);
+int convert_dec_to_bin(int n);
+int print_binary(va_list args);
+int print_octal(va_list args);
+int convert_dec_to_octal(int n);
+int print_u_int(va_list args);
+int print_hex_a(va_list args);
+int print_hex_A(va_list args);
+
+#endif
