25e994ebe0e8d9df41f10a1c714e94e8c32c8903
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2024-02-14 14:12:01 +0100

    Create _putchar.c

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - prints a character to standard output
+ * @c: character to print
+ *
+ */
+void _putchar(char c)
+{
+	char add = c;
+
+	write(STDOUT_FILENO, &add, sizeof(char));
+}
